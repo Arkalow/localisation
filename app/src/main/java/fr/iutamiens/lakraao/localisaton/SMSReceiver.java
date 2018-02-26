649a3abf30a1f77f27dfb559f2ca9420f7a5959c
Lakraa Omer <omer.lakraa@etud.u-picardie.fr>
2018-02-26 21:10:25 +0100

    Ajout de la classe SMSReceiver

---
+++ app/src/main/java/fr/iutamiens/lakraao/localisaton/SMSReceiver.java
@@ -0,0 +1,17 @@
+package fr.iutamiens.lakraao.localisaton;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by omer on 26/02/18.
+ */
+
+public class SMSReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
