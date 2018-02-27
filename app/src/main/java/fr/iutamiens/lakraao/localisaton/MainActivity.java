cbe6c21425617d0c087d585c5cfa8c7ed4a8dcb9
Lakraa Omer <omer.lakraa@etud.u-picardie.fr>
2018-02-27 18:32:26 +0100

    Récupération de la position avec update

--- app/src/main/java/fr/iutamiens/lakraao/localisaton/MainActivity.java
+++ app/src/main/java/fr/iutamiens/lakraao/localisaton/MainActivity.java
@@ -1,88 +1,91 @@
 package fr.iutamiens.lakraao.localisaton;
 
 import android.Manifest;
-import android.content.Context;
-import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.location.LocationProvider;
 import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
+    private LocationManager locationManager;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-
-        Criteria critere = new Criteria();
-
-        // Pour indiquer la précision voulue
-        // On peut mettre ACCURACY_FINE pour une haute précision ou ACCURACY_COARSE pour une moins bonne précision
-                critere.setAccuracy(Criteria.ACCURACY_FINE);
-
-        // Est-ce que le fournisseur doit être capable de donner une altitude ?
-                critere.setAltitudeRequired(false);
-
-        // Est-ce que le fournisseur doit être capable de donner une direction ?
-                critere.setBearingRequired(false);
-
-        // Est-ce que le fournisseur peut être payant ?
-                critere.setCostAllowed(false);
-
-        // Pour indiquer la consommation d'énergie demandée
-        // Criteria.POWER_HIGH pour une haute consommation, Criteria.POWER_MEDIUM pour une consommation moyenne et Criteria.POWER_LOW pour une basse consommation
-                critere.setPowerRequirement(Criteria.POWER_HIGH);
-
-        // Est-ce que le fournisseur doit être capable de donner une vitesse ?
-                critere.setSpeedRequired(false);
-
-        String provider = locationManager.getBestProvider(critere, true);
-        Log.d("Location", provider);
-
-        int permission = PermissionChecker.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
-        if (permission == PermissionChecker.PERMISSION_GRANTED) {
-        locationManager.requestLocationUpdates(provider, 5000, 10000, new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                Log.d("Location", "Locationchanged");
-            }
-
-            @Override
-            public void onStatusChanged(String s, int i, Bundle bundle) {
-                Log.d("Location", "Status changed");
-            }
-
-            @Override
-            public void onProviderEnabled(String s) {
-                Log.d("Location", "Enabled");
-            }
-
-            @Override
-            public void onProviderDisabled(String s) {
-                Log.d("Location", "Disabled");
-            }
-        });
-
-
-            Location location = locationManager.getLastKnownLocation(provider);
-            Log.d("Location", location.getLatitude()+"");
-            Log.d("Location", location.getLongitude()+"");
-        } else {
-            Log.e("Location", "Pas de permission");
-
-        }
-
-
-
     }
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        //Obtention de la référence du service
+        locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+
+        //Si le GPS est disponible, on s'y abonne
+        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            abonnementGPS();
+        }
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        //On appelle la méthode pour se désabonner
+        desabonnementGPS();
+    }
+
+    /**
+     * Méthode permettant de s'abonner à la localisation par GPS.
+     */
+    public void abonnementGPS() {
+        //On s'abonne
+        int permission = PermissionChecker.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+        if (permission == PermissionChecker.PERMISSION_GRANTED)
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, this);
+        Log.d("Localisaton", "abonement");
+    }
+
+    /**
+     * Méthode permettant de se désabonner de la localisation par GPS.
+     */
+    public void desabonnementGPS() {
+        //Si le GPS est disponible, on s'y abonne
+        locationManager.removeUpdates(this);
+        Log.d("Localisaton", "desabonement");
+    }
+
+    @Override
+    public void onLocationChanged(final Location location) {
+        //On affiche dans un Toat la nouvelle Localisation
+        final StringBuilder msg = new StringBuilder("lat : ");
+        msg.append(location.getLatitude());
+        msg.append( "; lng : ");
+        msg.append(location.getLongitude());
+        Log.d("Localisaton", msg.toString());
+        Toast.makeText(this, msg.toString(), Toast.LENGTH_SHORT).show();
+        desabonnementGPS();
+    }
+
+    @Override
+    public void onProviderDisabled(final String provider) {
+        //Si le GPS est désactivé on se désabonne
+        if("gps".equals(provider)) {
+            desabonnementGPS();
+        }
+    }
+
+    @Override
+    public void onProviderEnabled(final String provider) {
+        //Si le GPS est activé on s'abonne
+        if("gps".equals(provider)) {
+            abonnementGPS();
+        }
+    }
+
+    @Override
+    public void onStatusChanged(final String provider, final int status, final Bundle extras) { }
 }
